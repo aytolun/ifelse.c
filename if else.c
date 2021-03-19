6c39b8eedde716507bad6768e0ec5e38e60278f3
aytolun <78691060+aytolun@users.noreply.github.com>
2021-03-19 10:42:24 +0300

    if else

---
+++ if else.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+ 
+int main(){
+ 
+ int a=58, b=105;
+ 
+ 
+ //ifade dogruysa if, yanlýs ise else calýsacak
+ 
+ if(a>b){
+
+ 	printf("a:%d buyuktur", a);
+ 	
+ }
+  else {
+	
+	printf("b:%d buyuktur", b);
+	
+}
+
+
+return 0;
+}
